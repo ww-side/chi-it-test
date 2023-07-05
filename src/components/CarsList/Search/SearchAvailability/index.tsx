8be7d7c796ce5974015f473052fadac97be08d0a
ww_side <daniil.metelia@gmail.com>
2023-07-05 20:19:10 +0300

    fixed redirect to the first page after selection availability

--- src/components/CarsList/Search/SearchAvailability/index.tsx
+++ src/components/CarsList/Search/SearchAvailability/index.tsx
@@ -1,16 +1,18 @@
 import { FC } from 'react';
 import Select from '../../../UI/Select/index.tsx';
 import { carsSlice } from '../../../../store/reducers/carsSlice.ts';
+import { paginationSlice } from '../../../../store/reducers/paginationSlice.ts';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux.ts';
 
 const SearchAvailability: FC = () => {
   const { selectedAvailability } = useAppSelector(state => state.cars);
-
+  const { setCurrentPage } = paginationSlice.actions;
   const { setSelectedAvailability } = carsSlice.actions;
   const dispatch = useAppDispatch();
 
   const handleSelectedAvailabilityChange = (value: string) => {
     dispatch(setSelectedAvailability(value));
+    dispatch(setCurrentPage(1));
   };
 
   return (
